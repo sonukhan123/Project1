abc87ce3933ba90c1af42ecb3b98e92b721fbc61
LENOVO -Git <khan60939@gmail.com>
2022-04-24 11:43:10 +0530

    after changes

--- f1.c
+++ f1.c
@@ -5,7 +5,7 @@ fp=fopen("one.txt","w");
 if(fp==NULL)
     printf("not done");
 char *p;
-printf("Enter a string\n");
+printf("Enter a string to be written to the file\n");
 gets(p);
 fputs(p,fp);
 
