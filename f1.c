46d12dec5c61881ceca8d5f2020f3dce9c4e7e3d
LENOVO -Git <khan60939@gmail.com>
2022-04-25 00:15:17 +0530

    SDs

--- f1.c
+++ f1.c
@@ -1,13 +1,1 @@
-#include<stdio.h>
-int main(){
-FILE *fp;
-fp=fopen("one.txt","w");
-if(fp==NULL)
-    printf("not done");
-char *p;
-printf("Enter a string\n");
-gets(p);
-fputs(p,fp);
-
-fclose(fp);
-}
+sfsd
